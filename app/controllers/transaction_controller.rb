115f69c118ef3f735f6d99ddac6f9e2496283c7d
RobO1987 <robertong8@gmail.com>
2017-03-27 21:45:55 +0000

    Transaction Controller Updates

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -3,7 +3,6 @@ require 'uri'
 
 class TransactionController < ApplicationController
 
-<<<<<<< HEAD
   def index
   render "/transaction/purchase"
   end
@@ -13,13 +12,4 @@ class TransactionController < ApplicationController
     @result = HTTParty.get("http://marketdata.websol.barchart.com/getHistory.json?key=c259a86b4ec1a63d89b1dcc5173c24c1&symbol=#{symbol}&type=daily&startDate=20160327000000")
     render "/transaction/results"
   end
-=======
-<<<<<<< HEAD
-  def homepage
-  end
-=======
-
-  
->>>>>>> 3fca587d07057d280051a386dc44fb34a1edf345
->>>>>>> 137f57456903f38a6d9dbc344b1195640b00cbef
 end
