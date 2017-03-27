3e58b5402d7de471bc9e62b9f3b067538c8d1a3c
Jonathan Benstent <jonbenstent@gmail.com>
2017-03-27 22:36:04 +0000

    Fixed login information and error catch API

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -5,15 +5,40 @@ class TransactionController < ApplicationController
   end
 
   def search
-    symbol= params[:ticker]
+
+
+
+    symbol = params[:ticker]
+
+
 
     #HISTORICAL
     @historical_result = HTTParty.get("http://marketdata.websol.barchart.com/getHistory.json?key=c259a86b4ec1a63d89b1dcc5173c24c1&symbol=#{symbol}&type=daily&startDate=20160327000000")
 
-    #GET QUOTE
-    @current_result =
-    HTTParty.get("http://marketdata.websol.barchart.com/getQuote.json?key=c259a86b4ec1a63d89b1dcc5173c24c1&symbols=#{symbol}")
-    render "/transaction/results"
-  end
+    if !@historical_result
+      errors[:flash] = ['Ticker not found']
+      redirect_to "/"
+    else
+      @historical_result = HTTParty.get("http://marketdata.websol.barchart.com/getHistory.json?key=c259a86b4ec1a63d89b1dcc5173c24c1&symbol=#{symbol}&type=daily&startDate=20160327000000")
+    end
 
+    #GET QUOTE
+    @current_result =HTTParty.get("http://marketdata.websol.barchart.com/getQuote.json?key=c259a86b4ec1a63d89b1dcc5173c24c1&symbols=#{symbol}")
+    if @current_result.nil?
+      flash[:errors] = ['Ticker not found']
+      redirect_to "/"
+    elsif @current_result['results'].nil?
+      flash[:errors] = ['Ticker not found']
+      redirect_to "/"
+    elsif @current_result['results'][0].nil?
+      flash[:errors] = ['Ticker not found']
+      redirect_to "/"
+    elsif @current_result['results'][0]['name'].nil?
+      flash[:errors] = ['Ticker not found']
+      redirect_to "/"
+    else
+      @current_result =HTTParty.get("http://marketdata.websol.barchart.com/getQuote.json?key=c259a86b4ec1a63d89b1dcc5173c24c1&symbols=#{symbol}")
+      render "/transaction/results"
+    end
+  end
 end
