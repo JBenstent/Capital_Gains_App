358f4d5272639d4268b38e2b2defeb965a38c2d5
RobO1987 <robertong8@gmail.com>
2017-03-27 15:36:49 +0000

    Models build for Capital Gains App

---
+++ db/migrate/20170327153423_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string, :email
+      t.string :password_digest
+      t.integer :checking_account
+      t.string :address
+      t.string :city
+      t.string :state
+      t.integer :zip
+      t.integer :phone
+
+      t.timestamps null: false
+    end
+  end
+end
